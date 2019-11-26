ef9b13306ab649db3c156e11617dac2c0ad1f09d
everythingeverywhere <ivan0tarin@gmail.com>
2019-11-26 14:50:48 -0700

    init commit

---
+++ boot-kpack-demo/src/main/java/com/seroter/demo/GreetingController.java
@@ -0,0 +1,13 @@
+package com.seroter.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootKpackDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootKpackDemoApplication.class, args);
+	}
+
+}
