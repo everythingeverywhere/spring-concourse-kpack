6c63790cc0bf3df4dff423d5ed7eecc1f3c1bb01
everythingeverywhere <ivan0tarin@gmail.com>
2019-12-12 16:37:54 -0700

    updating tests

--- boot-kpack-demo/src/test/java/com/seroter/demo/BootKpackDemoApplicationTests.java
+++ boot-kpack-demo/src/test/java/com/seroter/demo/BootKpackDemoApplicationTests.java
@@ -1,16 +1,31 @@
+
 package com.seroter.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class BootKpackDemoApplicationTests {
 
+	@LocalServerPort
+    private int port;
+	
+	@Autowired
+    private TestRestTemplate restTemplate;
+	
 	@Test
-	public void contextLoads() {
+	public void testEndpoint() {
+		//assertThat(controller).isNotNull();
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
+                String.class)).contains("Hello");
 	}
 
 }
